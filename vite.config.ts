54dfcf42512fd55bd81be7a49239734be808c8fb
Henrique Hupp <henriquehupp@MBA.local>
2025-08-24 09:55:58 -0300

    style: set global style and variables

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use '@/assets/main' as *;
+        @use '@/assets/colors' as *;
+        @use '@/assets/transitions' as *;
+        @use '@/assets/variables' as *;
+        `,
+      },
     },
   },
 })
