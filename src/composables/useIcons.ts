fcfb730f68a4f0dd8309d561f45d832df61cd5f4
Henrique Hupp <henriquehupp@MBA.local>
2025-08-25 21:07:14 -0300

    feat: useIcons composable

---
+++ src/composables/useIcons.ts
@@ -0,0 +1,25 @@
+import LightAddressCard from '/icons/Light-address-card.svg'
+import LightEnvelope from '/icons/Light-envelope.svg'
+import LightEye from '/icons/Light-eye.svg'
+import LightFax from '/icons/Light-fax.svg'
+import LightLocationCheck from '/icons/Light-location-check.svg'
+import LightPhone from '/icons/Light-phone.svg'
+import LightUser from '/icons/Light-user.svg'
+import InfoTip from '/icons/info-tip.svg'
+
+export const icons = {
+  addressCard: LightAddressCard,
+  envelope: LightEnvelope,
+  eye: LightEye,
+  fax: LightFax,
+  locationCheck: LightLocationCheck,
+  phone: LightPhone,
+  user: LightUser,
+  infoTip: InfoTip,
+}
+
+type IconName = keyof typeof icons
+
+export const useIcons = (iconName: IconName): string => {
+  return icons[iconName]
+}
