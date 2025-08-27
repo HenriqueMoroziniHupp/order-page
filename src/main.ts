0087f7e7b331c4fdf9202a1918436885a0fdfc9f
Henrique Hupp <henriquehupp@MBA.local>
2025-08-27 18:17:30 -0300

    feat: add google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './assets/normalize.css'
-
+import { configure } from 'vue-gtag'
 import App from './App.vue'
 import router from './router'
 
+import './assets/normalize.css'
+
+configure({
+  tagId: 'G-ERT6R36PBS',
+})
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
